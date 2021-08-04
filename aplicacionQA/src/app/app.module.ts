8a5e5bc35eb75eca51c80cea11ab7cfa2085674a
florcorvalan <florcorvalan26@gmail.com>
2021-08-04 00:13:13 -0300

    frontend

---
+++ aplicacionQA/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    RegisterComponent,
+    BienvenidaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
